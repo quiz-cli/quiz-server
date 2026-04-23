f628d4836b150bd7c4dac50362ba346f2ef44cf6
Nina Belyavskaya <ninabel@users.noreply.github.com>
2026-04-23 18:34:59 +0200

    Bugfix with game state (#14)
    * #10 bugfix, server should not try to send messages when quiz is over
    
    * #10 bugfix, server should not try to send messages when quiz is over

--- src/main.py
+++ src/main.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 app = FastAPI()
 app.state.players = Players()
 app.state.results = Results()
+app.state.in_progress = False
 
 logging.basicConfig(
     level=logging.INFO,
@@ -69,7 +70,8 @@ async def connect(ws: WebSocket, player_name: str) -> None:
     except WebSocketDisconnect:
         logger.info("Player disconnects: %s", player_name)
         app.state.players.remove(player)
-        await app.state.admin.send_text(f"Player {player_name} disconnected")
+        if app.state.in_progress:
+            await app.state.admin.send_text(f"Player {player_name} disconnected")
 
 
 @app.websocket("/admin")
@@ -78,6 +80,7 @@ async def admin(ws: WebSocket) -> None:
     await ws.accept()
     quiz_data = await ws.receive_json()
     app.state.quiz = Quiz(**quiz_data)
+    app.state.in_progress = True
 
     app.state.admin = ws
 
@@ -102,6 +105,7 @@ async def admin(ws: WebSocket) -> None:
                 app.state.results.remove_results()
 
                 msg = "Quiz ended"
+                app.state.in_progress = False
                 await app.state.players.close_connection(msg)
                 await ws.close(reason=msg)
                 return
