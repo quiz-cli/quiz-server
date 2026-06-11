7264991462ec3ebd173d273b604ea796c26fa5be
Jana D <drazkova.j@gmail.com>
2026-06-11 20:41:56 +0200

    Iteration is done over the copy of the list

--- src/models.py
+++ src/models.py
@@ -6,6 +6,7 @@ from typing import ClassVar
 from fastapi import WebSocket
 from pydantic import BaseModel, ConfigDict
 
+
 logger = logging.getLogger(__name__)
 
 
@@ -91,7 +92,7 @@ class Players:
 
     async def close_connection(self, msg: str) -> None:
         """Disconnect all players with the given reason."""
-        for player in self._players:
+        for player in self._players.copy():
             await player.close_connection(msg)
 
 
